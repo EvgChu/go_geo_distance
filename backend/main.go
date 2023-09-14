216f80f29bfeedec6e9fc5a94bbd4d85edc66609
KinChu <chuevgenii@gmail.com>
2023-09-14 19:45:38 +0300

    add polygon moscow

--- backend/main.go
+++ backend/main.go
@@ -23,9 +23,7 @@ var albums = []album{
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
 	router.GET("/albums/:id", getAlbumByID)
-	router.POST("/albums", postAlbums)
 
 	router.Run("backend:8080")
 }
