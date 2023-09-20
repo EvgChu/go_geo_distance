2dc50ca84e04c055c967ef850bd104aa179846d1
KinChu <chuevgenii@gmail.com>
2023-09-20 23:59:29 +0300

    addbed code

--- backend/yageoservice/yageoservice.go
+++ backend/yageoservice/yageoservice.go
@@ -1,1 +1,32 @@
 package yageoservice
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type YandexAPIResponse struct {
+	Latitude  float64
+	Longitude float64
+	Addr      string
+}
+
+func (response *YandexAPIResponse) String() string {
+	return fmt.Sprintf("Latitude: %f\nLongitude: %f\nAddress: %s", response.Latitude, response.Longitude, response.Addr)
+}
+
+const (
+	urlYandexAPI = "https://geocode-maps.yandex.ru/1.x"
+	apiKeyEnvVar = "YANDEX_API_KEY"
+)
+
+func makeRequest(address string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s?format=json&apikey=%s&geocode=%s", urlYandexAPI, os.Getenv(apiKeyEnvVar), address), nil)
+	if err != nil {
+		return nil, err
+	}
+	return http.DefaultClient.Do(req)
+}
+
+type YaGeoService struct{ apiKey string }
